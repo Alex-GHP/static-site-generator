efa5f75af550d46fd912aff38ac4d052636c20a3
Alex-GHP <alex.morariu.dev@gmail.com>
2024-05-22 14:16:21 +0300

    add: markdown to blocks

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from textnode import TextNode
+from markdown_blocks import (
+    markdown_to_blocks
+)
 
 def main():
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(node)
+    md = markdown_to_blocks("""
+                    This is **bolded** paragraph
+
+                    This is another paragraph with *italic* text and `code` here
+                    This is the same paragraph on a new line
+
+                    * This is a list
+                    * with items
+                """)
+    for m in md:
+        print(m)
 
 main()
