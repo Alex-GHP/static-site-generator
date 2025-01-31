ab3eec20eeb2c189edcb759b40d36449d4168094
Alex-GHP <alex.morariu.dev@gmail.com>
2025-01-31 19:12:26 +0200

    inline markdown

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_link
 
 def main():
-    node = TextNode("This is text with a `code block` `word`", TextType.TEXT)
-    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+    )
+    new_nodes = split_nodes_link([node])
     print(new_nodes)
 
 main()
